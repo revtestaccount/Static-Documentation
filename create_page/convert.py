9bd5b971bff1c8e9b03b935382b02ea3888d4980
ademps03 <ademps03@revenue.ie>
2025-05-14 14:45:37 +0100

    added auto toc generator to convert.py

--- create_page/convert.py
+++ create_page/convert.py
@@ -1,28 +1,56 @@
 import markdown
 from bs4 import BeautifulSoup
 
-with open("././md/pat.md", "r", encoding="utf-8") as input_file:
+with open("././md/toctest2.md", "r", encoding="utf-8") as input_file:
     text = input_file.read()
 html = markdown.markdown(text, extensions=['fenced_code', 'tables'])
 
 new_html = BeautifulSoup(html, "lxml")
 
-#print(new_html)
+tableOfContents = []
+id = ""
 
 h1 = new_html.find_all('h1')
 
 for tag in h1:
-    tag['class'] = "pmod"
+    tag['class'] = "document-title"
 
 h2 = new_html.find_all('h2')
 
 for tag in h2:
-    tag['class'] = "document-title"
+    id = tag.contents[0].replace(" ", "_").lower()
+    tag['class'] = "pmod"
+    tag['id'] = id
+    tableOfContents.append(id)
+
+h3 = new_html.find_all('h3')
+
+for tag in h3:
+    id = tag.contents[0].replace(" ", "_").lower()
+    tag['class'] = "pmod"
+    tag['id'] = id
+    tableOfContents.append(id)
+
+h4 = new_html.find_all('h4')
+
+for tag in h4:
+    id = tag.contents[0].replace(" ", "_").lower()
+    tag['class'] = "pmod"
+    tag['id'] = id
+    tableOfContents.append(id)
+
+for id in tableOfContents:
+    newAnchor = new_html.new_tag("a")
+    newAnchor['href'] = "javascript:void(0)"
+    newAnchor['onclick'] = "document.getElementById(\'" + id + "\').scrollIntoView()"
+    newAnchor.append(id)
+    new_html.append(newAnchor)
 
 #code = new_html.find_all('code')
+# <a href="javascript:void(0)" onclick="document.getElementById('here').scrollIntoView()">go here</a>
 
 #for tag in code:
 #    tag['class'] = "border border-info"
 
-with open("././md/pat.html", "w", encoding="utf-8", errors="xmlcharrefreplace") as output_file:
+with open("././md/toctest2.html", "w", encoding="utf-8", errors="xmlcharrefreplace") as output_file:
     output_file.write(str(new_html))
