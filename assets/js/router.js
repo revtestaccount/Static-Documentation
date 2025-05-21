2c355137b200e20b1a373f990347beca1420fd1a
ademps03 <ademps03@revenue.ie>
2025-05-21 10:22:54 +0100

    add new routing code

--- assets/js/router.js
+++ assets/js/router.js
@@ -14,12 +14,51 @@ const locationHandler = async () => {
     location = "/";
   }
 
+  let k = null;
+  let v = null;
+  let breakCheck = false;
+
+  for (key in routes) {
+    if (breakCheck) {
+      break;
+    }
+    k = key;
+    for (val in routes[key]) {
+      if (val == location) {
+        v = routes[key];
+        breakCheck = true;
+        break;
+      }
+    }
+  }
+
+  console.log(k);
+  console.log(v);
+  let r = "";
+
+  for (e in v) {
+    console.log(v[e].message);
+
+
+    href = v[e].title.toLowerCase();
+    href = href.replaceAll(" ", "");
+    console.log(href);
+
+    r += "<a href=\"#" + href + "\">" + v[e].title + "</a>";
+  }
+
+  //<!-- <a href="#page1">Page 1</a> -->
+
   // get the route object from the routes object
-  const route = routes[location] || routes["404"];
+  const route = routes[k][location] || routes["404"];
   // get the html from the template
   const html = await fetch(route.template).then((response) => response.text());
   // set the content of the content div to the html
   document.getElementById("content").innerHTML = html;
+
+  if (location != "/" && location != "404") {
+    document.getElementById("sidebarLinks").innerHTML = r;
+  }
   // set the title of the document to the title of the route
   document.title = route.title;
 
