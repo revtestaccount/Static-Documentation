2f3eae9e26dc13c38e050f9d1e5a524d5598c089
sconnor0 <sconnor0@revenue.ie>
2026-06-04 12:41:35 +0100

    Replace sidebar with top navbar, add home page nav links

--- assets/js/router.js
+++ assets/js/router.js
@@ -33,8 +33,11 @@ const locationHandler = async () => {
     }
   }
 
-  // create html that will be inserted into the 'sidebarLinks' element on our page
+  // create html that will be inserted into the 'navLinksList' element on our page
   let sidebarLinksToRender = "";
+
+  // On the home page use the dedicated home_nav group for nav links
+  const navSource = (location === "/") ? jsonData["home_nav"] : routeValue;
 
   /* Code relating to the following:
    'soapschemareferencecurrentversion'
@@ -45,28 +48,28 @@ const locationHandler = async () => {
    Needs to be reworked. */
 
 
-  for (aRoute in routeValue) {
-    href = routeValue[aRoute].title.toLowerCase();
+  for (aRoute in navSource) {
+    href = navSource[aRoute].title.toLowerCase();
     href = href.replaceAll(" ", "");
 
     if (href != 'home' && href != '404pagenotfound' && href != 'soapschemareferencecurrentversion' && href != 'soapschemareferencenextversion' && href != 'restapireferencecurrentversion' && href != 'restapireferencenextversion') {
-      sidebarLinksToRender += "<a href=\"#" + href + "\">" + routeValue[aRoute].title + "</a>";
+      sidebarLinksToRender += "<li class=\"nav-item\"><a class=\"nav-link site-nav__link\" href=\"#" + aRoute + "\">" + navSource[aRoute].title + "</a></li>";
     }
 
     if (href == "soapschemareferencecurrentversion") {
-      sidebarLinksToRender += "<a href=\"./templates/schemas/PIT3/soap/soap-schema-reference/webframe.html\" target=\"_blank\" rel=\"noopener noreferrer\">SOAP Schema Reference Current Version</a>"
+      sidebarLinksToRender += "<li class=\"nav-item\"><a class=\"nav-link site-nav__link\" href=\"./templates/schemas/PIT3/soap/soap-schema-reference/webframe.html\" target=\"_blank\" rel=\"noopener noreferrer\">SOAP Schema Reference Current Version</a></li>";
     }
 
     if (href == "soapschemareferencenextversion") {
-      sidebarLinksToRender += "<a href=\"./templates/schemas/PIT4/soap/soap-schema-reference/webframe.html\" target=\"_blank\" rel=\"noopener noreferrer\">SOAP Schema Reference Next Version</a>"
+      sidebarLinksToRender += "<li class=\"nav-item\"><a class=\"nav-link site-nav__link\" href=\"./templates/schemas/PIT4/soap/soap-schema-reference/webframe.html\" target=\"_blank\" rel=\"noopener noreferrer\">SOAP Schema Reference Next Version</a></li>";
     }
 
     if (href == "restapireferencecurrentversion") {
-      sidebarLinksToRender += "<a href=\"./templates/schemas/PIT3/rest/paye-employers-rest-api-pit3.html\" target=\"_blank\" rel=\"noopener noreferrer\">REST API Reference Current Version</a>"
+      sidebarLinksToRender += "<li class=\"nav-item\"><a class=\"nav-link site-nav__link\" href=\"./templates/schemas/PIT3/rest/paye-employers-rest-api-pit3.html\" target=\"_blank\" rel=\"noopener noreferrer\">REST API Reference Current Version</a></li>";
     }
 
     if (href == "restapireferencenextversion") {
-      sidebarLinksToRender += "<a href=\"./templates/schemas/PIT4/rest/paye-employers-rest-api-pit4.html\" target=\"_blank\" rel=\"noopener noreferrer\">REST API Reference Next Version</a>"
+      sidebarLinksToRender += "<li class=\"nav-item\"><a class=\"nav-link site-nav__link\" href=\"./templates/schemas/PIT4/rest/paye-employers-rest-api-pit4.html\" target=\"_blank\" rel=\"noopener noreferrer\">REST API Reference Next Version</a></li>";
     }
 
   }
@@ -78,7 +81,7 @@ const locationHandler = async () => {
   // set the content of the content div to the html
   document.getElementById("content").innerHTML = html;
 
-  document.getElementById("sidebarLinks").innerHTML = sidebarLinksToRender;
+  document.getElementById("navLinksList").innerHTML = sidebarLinksToRender;
   // set the title of the document to the title of the route
   document.title = route.title;
 
